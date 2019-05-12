4d83c21b3c416d70efe0cd8bcc6beabfb680b6d0
fablecode <fablecode@users.noreply.github.com>
2019-05-12 22:44:10 +0100

    FileSystemService code coverage.

---
+++ src/wikia/processor/imageprocessor/src/Tests/Unit/imageprocessor.domain.unit.tests/ServicesTests/FileSystemServiceTests/DeleteTests.cs
@@ -0,0 +1,37 @@
+﻿using cardprocessor.tests.core;
+using imageprocessor.domain.Services;
+using imageprocessor.domain.SystemIO;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace imageprocessor.domain.unit.tests.ServicesTests.FileSystemServiceTests
+{
+    [TestFixture]
+    [Category(TestType.Unit)]
+    public class DeleteTests
+    {
+        private IFileSystem _fileSystem;
+        private FileSystemService _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fileSystem = Substitute.For<IFileSystem>();
+            _sut = new FileSystemService(_fileSystem);
+        }
+
+        [Test]
+        public void Given_A_LocalFileFullPath_Should_Invoke_Delete_Method_Once()
+        {
+            // Arrange
+            const string localFileFullPath = @"c:\images\pic.png";
+
+            // Act
+            _sut.Delete(localFileFullPath);
+
+            // Assert
+            _fileSystem.Received(1).Delete(Arg.Is(localFileFullPath));
+        }
+
+    }
+}
---
+++ src/wikia/processor/imageprocessor/src/Tests/Unit/imageprocessor.domain.unit.tests/ServicesTests/FileSystemServiceTests/GetFilesTests.cs
@@ -0,0 +1,38 @@
+﻿using cardprocessor.tests.core;
+using imageprocessor.domain.Services;
+using imageprocessor.domain.SystemIO;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace imageprocessor.domain.unit.tests.ServicesTests.FileSystemServiceTests
+{
+    [TestFixture]
+    [Category(TestType.Unit)]
+    public class GetFilesTests
+    {
+        private IFileSystem _fileSystem;
+        private FileSystemService _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fileSystem = Substitute.For<IFileSystem>();
+            _sut = new FileSystemService(_fileSystem);
+        }
+
+        [Test]
+        public void Given_A_Path_And_SearchPattern_Should_Invoke_GetFiles_Method_Once()
+        {
+            // Arrange
+            const string path = @"c:\images\pic.png";
+            const string searchPattern = "*pic";
+
+            // Act
+            _sut.GetFiles(path, searchPattern);
+
+            // Assert
+            _fileSystem.Received(1).GetFiles(Arg.Is(path), searchPattern);
+        }
+
+    }
+}
