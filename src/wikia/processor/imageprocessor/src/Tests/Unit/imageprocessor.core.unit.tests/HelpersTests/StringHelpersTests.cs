1e0e92ce50c39cb80f27378fd1d89bfd2fc53917
fablecode <fablecode@users.noreply.github.com>
2019-05-12 23:15:05 +0100

    SanitizeFileName code coverage.

---
+++ src/wikia/processor/imageprocessor/src/Tests/Unit/imageprocessor.core.unit.tests/HelpersTests/StringHelpersTests.cs
@@ -0,0 +1,27 @@
+﻿using cardprocessor.tests.core;
+using FluentAssertions;
+using imageprocessor.core.Helpers;
+using NUnit.Framework;
+
+namespace imageprocessor.core.unit.tests.HelpersTests
+{
+    [TestFixture]
+    [Category(TestType.Unit)]
+    public class StringHelpersTests
+    {
+        [TestCase("image*.png", "image.png")]
+        [TestCase("image<.png", "image.png")]
+        [TestCase("image>.png", "image.png")]
+        [TestCase("image|.png", "image.png")]
+        public void Given_An_Invalid_FileName_Should_Return_Santized_FileName(string filename, string expected)
+        {
+            // Arrange
+            // Act
+            var result = filename.SanitizeFileName();
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+
+    }
+}
