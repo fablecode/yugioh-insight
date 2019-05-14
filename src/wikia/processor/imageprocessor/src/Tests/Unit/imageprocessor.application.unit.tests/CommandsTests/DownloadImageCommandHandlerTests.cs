88a4807a14295bee0b2f7a4378674c7ff39f6f2e
fablecode <fablecode@users.noreply.github.com>
2019-05-14 21:43:58 +0100

    DownloadImageCommandHandler code coverage.

--- src/wikia/processor/imageprocessor/src/Tests/Unit/imageprocessor.application.unit.tests/CommandsTests/DownloadImageCommandHandlerTests.cs
+++ src/wikia/processor/imageprocessor/src/Tests/Unit/imageprocessor.application.unit.tests/CommandsTests/DownloadImageCommandHandlerTests.cs
@@ -1,9 +1,11 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
 using FluentValidation;
 using FluentValidation.Results;
 using imageprocessor.application.Commands.DownloadImage;
+using imageprocessor.core.Models;
 using imageprocessor.core.Services;
 using imageprocessor.tests.core;
 using NSubstitute;
@@ -54,6 +56,117 @@ namespace imageprocessor.application.unit.tests.CommandsTests
 
             // Assert
             result.Errors.Should().NotBeNullOrEmpty();
+        }
+
+        [Test]
+        public async Task Given_An_Valid_DownloadImageCommand_Should_Process_Command_Successfully()
+        {
+            // Arrange
+            var downloadImageCommand = new DownloadImageCommand
+            {
+                ImageFileName = "Adreus, Keeper of Armageddon",
+                ImageFolderPath = @"D:\Apps\ygo-api\Images\Cards",
+                RemoteImageUrl = new Uri("https://vignette.wikia.nocookie.net/yugioh/images/f/f6/AdreusKeeperofArmageddon-BP01-EN-R-1E.png")
+
+            };
+
+            _validator.Validate(Arg.Is(downloadImageCommand)).Returns(new ValidationResult());
+            _fileSystemService.Download(Arg.Any<Uri>(), Arg.Any<string>()).Returns(new DownloadedFile{ ContentType = "image/png" });
+
+            // Act
+            var result = await _sut.Handle(downloadImageCommand, CancellationToken.None);
+
+            // Assert
+            result.IsSuccessful.Should().BeTrue();
+        }
+
+        [Test]
+        public async Task Given_An_Valid_DownloadImageCommand_Should_Invoke_Download_Method_Once()
+        {
+            // Arrange
+            var downloadImageCommand = new DownloadImageCommand
+            {
+                ImageFileName = "Adreus, Keeper of Armageddon",
+                ImageFolderPath = @"D:\Apps\ygo-api\Images\Cards",
+                RemoteImageUrl = new Uri("https://vignette.wikia.nocookie.net/yugioh/images/f/f6/AdreusKeeperofArmageddon-BP01-EN-R-1E.png")
+
+            };
+
+            _validator.Validate(Arg.Is(downloadImageCommand)).Returns(new ValidationResult());
+            _fileSystemService.Download(Arg.Any<Uri>(), Arg.Any<string>()).Returns(new DownloadedFile{ ContentType = "image/png" });
+
+            // Act
+            await _sut.Handle(downloadImageCommand, CancellationToken.None);
+
+            // Assert
+            await _fileSystemService.Received(1).Download(Arg.Any<Uri>(), Arg.Any<string>());
+        }
+
+        [Test]
+        public async Task Given_An_Valid_DownloadImageCommand_Should_Invoke_Exists_Method_Once()
+        {
+            // Arrange
+            var downloadImageCommand = new DownloadImageCommand
+            {
+                ImageFileName = "Adreus, Keeper of Armageddon",
+                ImageFolderPath = @"D:\Apps\ygo-api\Images\Cards",
+                RemoteImageUrl = new Uri("https://vignette.wikia.nocookie.net/yugioh/images/f/f6/AdreusKeeperofArmageddon-BP01-EN-R-1E.png")
+
+            };
+
+            _validator.Validate(Arg.Is(downloadImageCommand)).Returns(new ValidationResult());
+            _fileSystemService.Download(Arg.Any<Uri>(), Arg.Any<string>()).Returns(new DownloadedFile{ ContentType = "image/png" });
+
+            // Act
+            await _sut.Handle(downloadImageCommand, CancellationToken.None);
+
+            // Assert
+            _fileSystemService.Received(1).Exists(Arg.Any<string>());
+        }
+
+        [Test]
+        public async Task Given_An_Valid_DownloadImageCommand_If_Image_Does_Not_Exist_Should_Not_Invoke_Delete_Method()
+        {
+            // Arrange
+            var downloadImageCommand = new DownloadImageCommand
+            {
+                ImageFileName = "Adreus, Keeper of Armageddon",
+                ImageFolderPath = @"D:\Apps\ygo-api\Images\Cards",
+                RemoteImageUrl = new Uri("https://vignette.wikia.nocookie.net/yugioh/images/f/f6/AdreusKeeperofArmageddon-BP01-EN-R-1E.png")
+
+            };
+
+            _validator.Validate(Arg.Is(downloadImageCommand)).Returns(new ValidationResult());
+            _fileSystemService.Download(Arg.Any<Uri>(), Arg.Any<string>()).Returns(new DownloadedFile{ ContentType = "image/png" });
+
+            // Act
+            await _sut.Handle(downloadImageCommand, CancellationToken.None);
+
+            // Assert
+            _fileSystemService.DidNotReceive().Delete(Arg.Any<string>());
+        }
+
+        [Test]
+        public async Task Given_An_Valid_DownloadImageCommand_If_Image_Exists_Should_Invoke_Delete_Method_Once()
+        {
+            // Arrange
+            var downloadImageCommand = new DownloadImageCommand
+            {
+                ImageFileName = "Adreus, Keeper of Armageddon",
+                ImageFolderPath = @"D:\Apps\ygo-api\Images\Cards",
+                RemoteImageUrl = new Uri("https://vignette.wikia.nocookie.net/yugioh/images/f/f6/AdreusKeeperofArmageddon-BP01-EN-R-1E.png")
+
+            };
+
+            _validator.Validate(Arg.Is(downloadImageCommand)).Returns(new ValidationResult());
+            _fileSystemService.Download(Arg.Any<Uri>(), Arg.Any<string>()).Returns(new DownloadedFile{ ContentType = "image/png" });
+            _fileSystemService.Exists(Arg.Any<string>()).Returns(true);
+
+            // Act
+            await _sut.Handle(downloadImageCommand, CancellationToken.None);
+
+            // Assert
+            _fileSystemService.Received(1).Delete(Arg.Any<string>());
         }
     }
 }
