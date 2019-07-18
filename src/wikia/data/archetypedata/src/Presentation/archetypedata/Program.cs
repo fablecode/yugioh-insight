e89986412266b0da80243a6c5581ea0654ec1566
fablecode <fablecode@users.noreply.github.com>
2019-07-18 22:20:58 +0100

    ArchetypeProcessor code coverage.

---
+++ src/wikia/data/archetypedata/src/Presentation/archetypedata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace archetypedata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
