e3d7a0629dc0e8d43e1972880961b4a477d2b1a8
fablecode <fablecode@users.noreply.github.com>
2019-07-22 07:48:57 +0100

    Infrastructure installer added.

---
+++ src/wikia/processor/archetypeprocessor/src/Infrastructure/archetypeprocessor.infrastructure/InfrastructureInstaller.cs
@@ -0,0 +1,34 @@
+﻿using archetypeprocessor.domain.Repository;
+using archetypeprocessor.infrastructure.Database;
+using archetypeprocessor.infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace archetypeprocessor.infrastructure
+{
+    public static class InfrastructureInstaller
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, string connectionString)
+        {
+            services.AddYgoDatabase(connectionString)
+                .AddRepositories();
+
+
+            return services;
+        }
+        public static IServiceCollection AddYgoDatabase(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<YgoDbContext>(c => c.UseSqlServer(connectionString));
+
+            return services;
+        }
+
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddTransient<IArchetypeRepository, ArchetypeRepository>();
+            services.AddTransient<IArchetypeCardsRepository, ArchetypeCardsRepository>();
+
+            return services;
+        }
+    }
+}
