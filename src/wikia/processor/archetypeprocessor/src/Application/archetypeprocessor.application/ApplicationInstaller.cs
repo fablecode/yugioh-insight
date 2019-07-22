7bb3f1d36bbb591baa31408a8c5f80d439b19cbc
fablecode <fablecode@users.noreply.github.com>
2019-07-22 08:21:12 +0100

    Application installer added.

---
+++ src/wikia/processor/archetypeprocessor/src/Application/archetypeprocessor.application/ApplicationInstaller.cs
@@ -0,0 +1,42 @@
+﻿using archetypeprocessor.core.Services;
+using archetypeprocessor.domain.Services;
+using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace archetypeprocessor.application
+{
+    public static class ApplicationInstaller
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services
+                .AddCqrs()
+                .AddDomainServices()
+                .AddAutoMapper(); 
+
+            return services;
+        }
+
+        private static IServiceCollection AddCqrs(this IServiceCollection services)
+        {
+            services.AddMediatR(typeof(ApplicationInstaller).GetTypeInfo().Assembly);
+
+            return services;
+        }
+
+        private static IServiceCollection AddDomainServices(this IServiceCollection services)
+        {
+            services.AddTransient<IArchetypeService, ArchetypeService>();
+            services.AddTransient<IArchetypeCardsService, ArchetypeCardsService>();
+
+            return services;
+        }
+
+        private static IServiceCollection AddValidation(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
--- src/wikia/processor/archetypeprocessor/src/Application/archetypeprocessor.application/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace archetypeprocessor.application
-{
-    public class Class1
-    {
-    }
-}
