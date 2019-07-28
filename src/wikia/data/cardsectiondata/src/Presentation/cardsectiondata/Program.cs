72aea88e979edd8ed138b5ef0c2f559d4a786af0
fablecode <fablecode@users.noreply.github.com>
2019-07-28 09:43:59 +0100

    cardsectiondata console app added.

---
+++ src/wikia/data/cardsectiondata/src/Presentation/cardsectiondata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cardsectiondata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
