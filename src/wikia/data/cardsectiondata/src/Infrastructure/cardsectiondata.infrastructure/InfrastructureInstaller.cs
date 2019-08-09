0ceeafb199fb3721c240faf1c8f5c03cb4d51684
fablecode <fablecode@users.noreply.github.com>
2019-08-09 11:14:51 +0100

    Infrastructure project added.

---
+++ src/wikia/data/cardsectiondata/src/Infrastructure/cardsectiondata.infrastructure/InfrastructureInstaller.cs
@@ -0,0 +1,24 @@
+﻿using cardsectiondata.domain.Services.Messaging;
+using cardsectiondata.infrastructure.Services.Messaging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace cardsectiondata.infrastructure
+{
+    public static class InfrastructureInstaller
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddMessagingServices();
+
+            return services;
+        }
+        public static IServiceCollection AddMessagingServices(this IServiceCollection services)
+        {
+            services.AddTransient<IQueue, CardTipsQueue>();
+            services.AddTransient<IQueue, CardRulingQueue>();
+            services.AddTransient<IQueue, CardTriviaQueue>();
+
+            return services;
+        }
+    }
+}
---
+++ src/wikia/data/cardsectiondata/src/Infrastructure/cardsectiondata.infrastructure/Services/Messaging/CardTriviaQueue.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using cardsectiondata.core.Constants;
+using cardsectiondata.core.Models;
+using cardsectiondata.domain.Services.Messaging;
+
+namespace cardsectiondata.infrastructure.Services.Messaging
+{
+    public class CardTriviaQueue : IQueue
+    {
+        public Task Publish(CardSectionMessage message)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Handles(string category)
+        {
+            return category == ArticleCategory.CardTrivia;
+        }
+    }
+}
