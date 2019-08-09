5ced050833c3925268777e87b1eec394203d45c5
fablecode <fablecode@users.noreply.github.com>
2019-08-09 11:13:07 +0100

    Fix category typo

---
+++ src/wikia/data/cardsectiondata/src/Core/cardsectiondata.core/Processor/ICardSectionProcessor.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using cardsectiondata.core.Models;
+
+namespace cardsectiondata.core.Processor
+{
+    public interface ICardSectionProcessor
+    {
+        Task<CardSectionMessage> ProcessItem(Article article);
+    }
+}
