cbf73b91dda843d61daaed8754733f6b6ecb0a4d
fablecode <fablecode@users.noreply.github.com>
2019-08-12 22:44:30 +0100

    Infrastructure installer added.

---
+++ src/wikia/processor/cardsectionprocessor/src/Infrastructure/cardsectionprocessor.infrastructure/InfrastructureInstaller.cs
@@ -0,0 +1,35 @@
+﻿using cardsectionprocessor.domain.Repository;
+using cardsectionprocessor.infrastructure.Database;
+using cardsectionprocessor.infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace cardsectionprocessor.infrastructure
+{
+    public static class InfrastructureInstaller
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, string connectionString)
+        {
+            services.AddYgoDatabase(connectionString);
+            services.AddRepositories();
+
+            return services;
+        }
+
+        public static IServiceCollection AddYgoDatabase(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContextPool<YgoDbContext>(c => c.UseSqlServer(connectionString));
+            return services;
+        }
+
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddTransient<ICardRepository, CardRepository>();
+            services.AddTransient<ICardTipRepository, CardTipRepository>();
+            services.AddTransient<ICardRulingRepository, CardRulingRepository>();
+            services.AddTransient<ICardTriviaRepository, CardTriviaRepository>();
+
+            return services;
+        }
+    }
+}
