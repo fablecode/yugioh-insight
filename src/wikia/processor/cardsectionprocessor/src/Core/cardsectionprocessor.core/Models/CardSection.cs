1a691a04b073f66fbd21f323d46276849f415ece
fablecode <fablecode@users.noreply.github.com>
2019-08-12 05:21:12 +0100

    cardsectionprocessor core models added.

---
+++ src/wikia/processor/cardsectionprocessor/src/Core/cardsectionprocessor.core/Models/CardSection.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace cardsectionprocessor.core.Models
+{
+    public class CardSection
+    {
+        public string Name { get; set; }
+        public List<string> ContentList { get; set; } = new List<string>();
+    }
+}
---
+++ src/wikia/processor/cardsectionprocessor/src/Core/cardsectionprocessor.core/Models/CardSectionMessage.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace cardsectionprocessor.core.Models
+{
+    public class CardSectionMessage
+    {
+        public string Name { get; set; }
+        public List<CardSection> CardSections { get; set; } = new List<CardSection>();
+    }
+}
