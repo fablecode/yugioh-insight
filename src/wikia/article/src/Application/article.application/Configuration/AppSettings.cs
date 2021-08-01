3be17443a929b212129d4aa59689000e21bb2e89
fablecode <fablecode@users.noreply.github.com>
2021-08-01 08:13:17 +0200

    Now using one configurable docker image, article.archetypes, to retrieve all archetype information.

--- src/wikia/article/src/Application/article.application/Configuration/AppSettings.cs
+++ src/wikia/article/src/Application/article.application/Configuration/AppSettings.cs
@@ -3,5 +3,7 @@
     public record AppSettings
     {
         public string WikiaDomainUrl { get; init; }
+        public string Category { get; init; }
+        public int PageSize { get; init; }
     }
 }
--- src/wikia/article/src/Presentation/article.archetypes/QuartzConfiguration/ArchetypeInformationJob.cs
+++ src/wikia/article/src/Presentation/article.archetypes/QuartzConfiguration/ArchetypeInformationJob.cs
@@ -2,6 +2,8 @@
 using MediatR;
 using Quartz;
 using System.Threading.Tasks;
+using article.application.Configuration;
+using Microsoft.Extensions.Options;
 
 namespace article.archetypes.QuartzConfiguration
 {
@@ -9,17 +11,16 @@ namespace article.archetypes.QuartzConfiguration
     public class ArchetypeInformationJob : IJob
     {
         private readonly IMediator _mediator;
+        private readonly IOptions<AppSettings> _options;
 
-        public ArchetypeInformationJob(IMediator mediator)
+        public ArchetypeInformationJob(IMediator mediator, IOptions<AppSettings> options)
         {
             _mediator = mediator;
+            _options = options;
         }
         public async Task Execute(IJobExecutionContext context)
         {
-            const int pageSize = 500;
-            const string category = "Archetypes";
-
-            await _mediator.Send(new ArchetypeInformationTask { Category = category, PageSize = pageSize });
+            await _mediator.Send(new ArchetypeInformationTask { Category = _options.Value.Category, PageSize = _options.Value.PageSize });
         }
     }
 }
