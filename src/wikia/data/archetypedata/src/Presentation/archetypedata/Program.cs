c88f80ec94bb9456d2cee2ddb8976529fc763600
fablecode <fablecode@users.noreply.github.com>
2021-08-02 22:44:16 +0200

    Upgraded all project files from .netcore2.2 to net5.0

--- src/wikia/data/archetypedata/src/Presentation/archetypedata/Program.cs
+++ src/wikia/data/archetypedata/src/Presentation/archetypedata/Program.cs
@@ -1,73 +1,46 @@
-﻿using System;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using archetypedata.application;
+﻿using archetypedata.application;
 using archetypedata.application.Configuration;
-using archetypedata.Extensions.WindowsService;
 using archetypedata.infrastructure;
-using archetypedata.Services;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Serilog;
+using System;
+using archetypedata.Services;
 
 namespace archetypedata
 {
     internal class Program
     {
-        static async Task Main(string[] args)
+        internal static void Main(string[] args)
         {
-            AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionTrapper;
-
-            var hostBuilder = CreateHostBuilder(args);
-
-            var isService = !(Debugger.IsAttached || args.Contains("--console"));
-
-            if (isService)
+            try
             {
-                var pathToExe = Process.GetCurrentProcess().MainModule?.FileName;
-                var pathToContentRoot = Path.GetDirectoryName(pathToExe);
-                Directory.SetCurrentDirectory(pathToContentRoot);
+                CreateHostBuilder(args).Build().Run();
             }
+            catch (Exception ex)
+            {
+                if (Log.Logger == null || Log.Logger.GetType().Name == "SilentLogger")
+                {
+                    Log.Logger = new LoggerConfiguration()
+                        .MinimumLevel.Debug()
+                        .WriteTo.Console()
+                        .CreateLogger();
+                }
 
-            if (isService)
-                await hostBuilder.RunAsServiceAsync();
-            else
-                await hostBuilder.RunConsoleAsync();
+                Log.Fatal(ex, "Host terminated unexpectedly");
+
+                throw;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
-        private static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            var hostBuilder = new HostBuilder()
-                .ConfigureLogging((hostContext, config) =>
-                {
-                    config.AddConsole();
-                    config.AddDebug();
-                })
-                .ConfigureHostConfiguration(config =>
-                {
-#if DEBUG
-                    config.AddEnvironmentVariables(prefix: "ASPNETCORE_");
-#else
-                        config.AddEnvironmentVariables();
-#endif
-                })
-                .ConfigureAppConfiguration((hostContext, config) =>
-                {
-                    config.SetBasePath(Directory.GetCurrentDirectory());
-                    config.AddJsonFile("appsettings.json", false, true);
-                    config.AddCommandLine(args);
-
-#if DEBUG
-                    config.AddUserSecrets<Program>();
-#endif
-                })
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
                 .ConfigureServices((hostContext, services) =>
                 {
-                    services.AddHttpClient();
                     services.AddLogging();
 
                     //configuration settings
@@ -80,16 +53,89 @@ namespace archetypedata
                     services.AddApplicationServices();
                     services.AddInfrastructureServices();
                 })
-                .UseConsoleLifetime()
-                .UseSerilog();
-
-            return hostBuilder;
-        }
-
-        private static void UnhandledExceptionTrapper(object sender, UnhandledExceptionEventArgs ex)
-        {
-            Log.Logger.Error("Unhandled exception occurred. Exception: {@Exception}", ex);
-        }
-
+                .UseSerilog((hostingContext, loggerConfiguration) =>
+                {
+                    loggerConfiguration
+                        .ReadFrom.Configuration(hostingContext.Configuration)
+                        .Enrich.WithProperty("ApplicationName", typeof(Program).Assembly.GetName().Name)
+                        .Enrich.WithProperty("Environment", hostingContext.HostingEnvironment);
+                });
     }
+
+    //    internal class Program
+    //    {
+    //        static async Task Main(string[] args)
+    //        {
+    //            AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionTrapper;
+
+    //            var hostBuilder = CreateHostBuilder(args);
+
+    //            var isService = !(Debugger.IsAttached || args.Contains("--console"));
+
+    //            if (isService)
+    //            {
+    //                var pathToExe = Process.GetCurrentProcess().MainModule?.FileName;
+    //                var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+    //                Directory.SetCurrentDirectory(pathToContentRoot);
+    //            }
+
+    //            if (isService)
+    //                await hostBuilder.RunAsServiceAsync();
+    //            else
+    //                await hostBuilder.RunConsoleAsync();
+    //        }
+
+    //        private static IHostBuilder CreateHostBuilder(string[] args)
+    //        {
+    //            var hostBuilder = new HostBuilder()
+    //                .ConfigureLogging((hostContext, config) =>
+    //                {
+    //                    config.AddConsole();
+    //                    config.AddDebug();
+    //                })
+    //                .ConfigureHostConfiguration(config =>
+    //                {
+    //#if DEBUG
+    //                    config.AddEnvironmentVariables(prefix: "ASPNETCORE_");
+    //#else
+    //                        config.AddEnvironmentVariables();
+    //#endif
+    //                })
+    //                .ConfigureAppConfiguration((hostContext, config) =>
+    //                {
+    //                    config.SetBasePath(Directory.GetCurrentDirectory());
+    //                    config.AddJsonFile("appsettings.json", false, true);
+    //                    config.AddCommandLine(args);
+
+    //#if DEBUG
+    //                    config.AddUserSecrets<Program>();
+    //#endif
+    //                })
+    //                .ConfigureServices((hostContext, services) =>
+    //                {
+    //                    services.AddHttpClient();
+    //                    services.AddLogging();
+
+    //                    //configuration settings
+    //                    services.Configure<AppSettings>(hostContext.Configuration.GetSection(nameof(AppSettings)));
+    //                    services.Configure<RabbitMqSettings>(hostContext.Configuration.GetSection(nameof(RabbitMqSettings)));
+
+    //                    // hosted service
+    //                    services.AddHostedService<ArchetypeDataHostedService>();
+
+    //                    services.AddApplicationServices();
+    //                    services.AddInfrastructureServices();
+    //                })
+    //                .UseConsoleLifetime()
+    //                .UseSerilog();
+
+    //            return hostBuilder;
+    //        }
+
+    //        private static void UnhandledExceptionTrapper(object sender, UnhandledExceptionEventArgs ex)
+    //        {
+    //            Log.Logger.Error("Unhandled exception occurred. Exception: {@Exception}", ex);
+    //        }
+
+    //    }
 }
