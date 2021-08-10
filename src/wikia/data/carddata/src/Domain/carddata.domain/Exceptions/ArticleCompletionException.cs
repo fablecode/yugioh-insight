27b014285b0c050855dd8ae8635dd24280f9ad2f
fablecode <fablecode@users.noreply.github.com>
2021-08-10 07:37:53 +0200

    Extracted ArticleCompletionException class to it's own file.

---
+++ src/wikia/data/carddata/src/Domain/carddata.domain/Exceptions/ArticleCompletionException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace carddata.domain.Exceptions
+{
+    public class ArticleCompletionException : Exception
+    {
+        public ArticleCompletionException(string exceptionMessage)
+            : base (exceptionMessage)
+        {
+        }
+    }
+}
