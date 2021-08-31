94c0877bc492164a4e57eb9a655c8fa7d31d1c0b
fablecode <fablecode@users.noreply.github.com>
2021-08-31 18:22:32 +0200

    Remove unecessary interfaces.

--- src/services/Cards.API/src/Domain/Cards.Domain/Models/Card.cs
+++ src/services/Cards.API/src/Domain/Cards.Domain/Models/Card.cs
@@ -11,44 +11,4 @@ namespace Cards.Domain.Models
             Name = name;
         }
     }
-
-    public interface ISpellCardType
-    {
-        string Name { get; }
-        string Type { get; }
-    }
-
-    public interface ITrapCardType
-    {
-        string Name { get; }
-        string Type { get; }
-    }
-
-    public interface IMonsterCard
-    {
-        string Name { get; }
-        string Attribute { get; }
-        string Type { get; }
-        string[] SubTypes { get; }
-    }
-
-    public interface IAttackAndDefence
-    {
-        int? Atk { get; }
-        int? Def { get; }
-    }
-
-    public interface ICard
-    {
-        ISpellCardType SpellCardType { get; }
-        string Name { get; }
-    }
-
-
-    public enum CardType
-    {
-        Monster,
-        Spell,
-        Trap
-    }
 }
